45694506de127300204323599d9cc1f388decec6
GMatrixGames <gmatrixgames@hotmail.com>
2021-02-03 12:40:34 -0500

    v1 item list

--- FortniteAPI-Wrapper/Models/v1/V1Images.cs
+++ FortniteAPI-Wrapper/Models/v1/V1Images.cs
@@ -5,6 +5,7 @@ namespace FortniteAPI.Models.v1
     public class V1Images
     {
         [JsonProperty("icon")] public string Icon;
+        [JsonProperty("full_size")] public string FullSize;
         [JsonProperty("featured")] public string Featured;
         [JsonProperty("background")] public string Background;
         [JsonProperty("full_background")] public string FullBackground;
--- FortniteAPI-Wrapper/Models/v1/V1Item.cs
+++ FortniteAPI-Wrapper/Models/v1/V1Item.cs
@@ -9,13 +9,17 @@ namespace FortniteAPI.Models.v1
         [JsonProperty("name")] public string Name;
         [JsonProperty("type")] public string Type;
         [JsonProperty("rarity")] public string Rarity;
+        [JsonProperty("internalRarity")] public string InternalRarity;
         [JsonProperty("set")] public string Set;
         [JsonProperty("price")] public int Price;
+        [JsonProperty("added")] public DateTime Added;
         [JsonProperty("interest")] public double Interest;
         [JsonProperty("releaseDate")] public DateTime ReleaseDate; // ex. 2020-09-11 | DateTime or string?
         [JsonProperty("description")] public string Description;
         [JsonProperty("reactive")] public bool Reactive;
         [JsonProperty("images")] public V1Images Images;
         [JsonProperty("videos")] public V1Videos[] Videos;
+        [JsonProperty("video")] public string Video;
+        [JsonProperty("gameplayTags")] public string[] GameplayTags;
     }
 }
