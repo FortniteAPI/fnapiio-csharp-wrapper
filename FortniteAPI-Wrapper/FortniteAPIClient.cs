6424169de1fcd5f0aebe674b1c52ccd9e63fa06c
GMatrixGames <gmatrixgames@hotmail.com>
2021-02-03 11:59:12 -0500

    Init wrapper

---
+++ FortniteAPI-Wrapper/FortniteAPIClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using FortniteAPI.Util;
+using RestSharp;
+
+namespace FortniteAPI
+{
+    public class FortniteAPIClient
+    {
+        
+        
+        public FortniteAPIClient(string api)
+        {
+            if (!string.IsNullOrWhiteSpace(api))
+            {
+                var ver = Assembly.GetExecutingAssembly().GetName().Version;
+                
+                var rest = new RestClient("https://fortniteapi.io")
+                {
+                    UserAgent = $"FortniteAPIIO/{ver?.ToString(3)}",
+                    Timeout = 10 * 1000
+                }.UseSerializer<JsonNetSerializer>();
+                
+                
+            }
+            else
+            {
+                throw new Exception("API is required, please supply an API key in the constructor");
+            }
+        }
+    }
+}
